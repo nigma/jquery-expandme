3d17e727f5bc50fa08149ef48a7381f2e9121441
Filip Wasilewski <en@ig.ma>
2011-12-02 20:06:10 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,65 @@
+# jQuery expandme plugin
+
+A jQuery JavaScript plugin for expanding images vertically in articles.
+
+## Info
+
+This is a very simple plugin that can be usefull when putting large images (i.e. website screenshots) inside articles.
+
+An image can occupy full width of an article while only the top part of it is visible. An expand/collapse bar at the bottom of the image view expands the image container vertically making the image fully visible.
+
+## Usage
+
+Assume we have an article with embedded image inside .expandme wrapper and defined stylesheet similar to the one in ``jquery.expandme\css\expandme.css``.
+
+    <article>
+        <div class="expandme" style="height: 440px;">
+            <img src="image.jpg" />
+            <a href="#" class="expand-toggle"></a>
+        </div>
+    </erticle>
+
+Then the corresponding JS bit is:
+
+    $('.expandme').expandme();
+
+
+The plugin can also take callbacks so for example you may want to scroll to article top when collapsing an image:
+
+    $('.expandme').expandme({
+        onBeforeCollapse: function(settings){
+            var $elem = $(this); // Expandable
+            // requires scrollTo plugin
+            $.scrollTo($elem.parents('article'), settings.speed);
+        }
+    });
+
+## Options
+
+All available plugin settings:
+
+    var settings = $.extend({
+        height: null,                    // Initial height of image (usually specified in css)
+        speed: 500,                      // Expansion/collapsing speed
+        expandEasing: 'swing',           // Expand easing for jQuery animate
+        collapseEasing: 'swing',         // Collapsing easing for jQuery animate
+        activeClass: 'active',           // Class added to expanded element
+        expandToggleClass: 'expand-toggle', // Class for anchor element that triggers expand action
+        onBeforeExpand: null,            // Function to call before expanding
+        onAfterExpand: null,             // Function to call after expanding
+        onBeforeCollapse: null,          // Function to call before collapsing
+        onAfterCollapse: null,           // Function to call after collapsing
+        contentSelector: 'img',          // jQuery selector string of full-height content element
+        namespace: 'expandme'            // Plugin data namespace
+    }, options);
+
+
+## Resources
+
+See the ``css`` and ``img`` for stylesheet and image resources required by the plugin.
+
+## Demo
+
+See the ``index.html`` file in demo folder or http://en.ig.ma/projects for examples.
+
+Read more at http://en.ig.ma/notebook/2011/jquery-expandme-plugin.
---
+++ jquery.expandme/jquery.expandme.js
@@ -0,0 +1,70 @@
+/*!
+ * jQuery ExpandMe Plugin v1.0
+ * http://en.ig.ma/notebook/2011/jquery-expandme-plugin
+ *
+ * Copyright 2011, Filip Wasilewski <en@ig.ma>
+ * Licensed under the MIT license.
+ */
+
+(function ($) {
+    $.fn.expandme = function (options) {
+        var settings = $.extend({
+            height: null,                    // Initial height of image (usually specified in css)
+            speed: 500,                      // Expansion/collapsing speed
+            expandEasing: 'swing',           // Expand easing for jQuery animate
+            collapseEasing: 'swing',         // Collapsing easing for jQuery animate
+            activeClass: 'active',           // Class added to expanded element
+            expandToggleClass: 'expand-toggle', // Class for anchor element that triggers expand action
+            onBeforeExpand: null,            // Function to call before expanding
+            onAfterExpand: null,             // Function to call after expanding
+            onBeforeCollapse: null,          // Function to call before collapsing
+            onAfterCollapse: null,           // Function to call after collapsing
+            contentSelector: 'img',          // jQuery selector string of full-height content element
+            namespace: 'expandme'            // Plugin data namespace
+        }, options);
+
+        this.css('height', settings.height);
+
+        return this.each(function () {
+            var $main = $(this);
+            var data = $main.data(settings.namespace);
+
+            // Initial wrapper height before expansion
+            var initialHeight = settings.height || $main.height();
+
+            // If the plugin hasn't been initialized yet
+            if(!data) {
+                $main.data(settings.namespace, {
+                    target:$main,
+                    height:settings.height
+                });
+                $('.' + settings.expandToggleClass, $main).toggle(
+                        function (event) {
+                            $main.addClass(settings.activeClass);
+                            var $content = $main.children(settings.contentSelector);
+                            var $toggle = $main.children('.' + settings.expandToggleClass);
+                            var toggleHeight = $toggle.height() || 0;
+                            var attr = {height:$content.height() + toggleHeight};
+                            if(settings.onBeforeExpand) {
+                                settings.onBeforeExpand.call($main, settings);
+                            }
+                            animate(attr, settings.expandEasing, settings.onAfterExpand);
+                        },
+                        function (event) {
+                            $main.removeClass(settings.activeClass);
+                            var attr = {height:initialHeight};
+                            if(settings.onBeforeCollapse) {
+                                settings.onBeforeCollapse.call($main, settings);
+                            }
+                            animate(attr, settings.collapseEasing, settings.onAfterCollapse);
+                        }
+                );
+                function animate(attr, easing, callback) {
+                    $main.animate(attr, settings.speed, easing, callback && function () {
+                        callback.call(this, settings);
+                    });
+                }
+            }
+        });
+    };
+})(jQuery);
